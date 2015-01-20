674419787239895e55b0ae4264319e83f03c0d3d
范小龙 <mynamexiaolong@gmail.com>
2015-01-20 14:07:04 +0800

    修改配置文件，增加到忽略列表

--- js/config.js
+++
@@ -1,9 +0,0 @@
-﻿define(function () {
-    var ipconfig = {
-        dcmpRESTfulIp: "http://172.16.135.124:8080/CloudDCSP",//
-        upLoadRESTfulIp: "http://172.16.135.124:9090",
-        downLoadRESTfulIp:""
-    };
-
-    return  ipconfig;
-});
